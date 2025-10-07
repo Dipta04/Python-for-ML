ea8e8aaf3a4ed3050d77ff9832f589a4f187ba58
Dipta04 <diptakarmakar38@gmail.com>
2025-10-07 14:32:22 +0600

    Initial commit

---
+++ Module_1/F_Digits_Summation.py
@@ -0,0 +1,14 @@
+inp = input()
+
+numbers = inp.split()
+
+x = int(numbers[0]) 
+
+y = int(numbers[1])
+
+last_digit_of_x =  x % 10
+last_digit_of_y = y % 10
+
+sum = last_digit_of_x + last_digit_of_y
+
+print(sum)
